a0b6e306aabcd4a1bd06a653fe90d60ecf9236f5
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2026-05-06 09:30:54 +0530

    Add: palindromic-substrings - submission-8

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-8.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int ans = 0;
+        int[] manacher = manacherArr(s);
+        for(int len : manacher) {
+            ans += (len + 1) / 2;
+        }
+        return ans;
+    }
+
+    private int[] manacherArr(String s) {
+        StringBuilder sb = new StringBuilder("#");
+
+        for(char ch : s.toCharArray()) {
+            sb.append(ch).append("#");
+        }
+
+        int n = sb.length();
+        int[] manacher = new int[n];
+        int l = 0, r = 0;
+
+        for(int i = 0; i < n; i++) {
+            manacher[i] = i < r ? Math.min(r - i, manacher[l + (r - i)]) : 0;
+            while(i + manacher[i] + 1 < n && i - manacher[i] - 1 >= 0 && 
+            sb.charAt(i + manacher[i] + 1) == sb.charAt(i - manacher[i] - 1)) {
+                manacher[i]++;
+            }
+            if(i + manacher[i] > r) {
+                l = i - manacher[i];
+                r = i + manacher[i];
+            }
+        }
+
+        return manacher;
+    }
+}
