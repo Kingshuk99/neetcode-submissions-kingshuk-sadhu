3ce4039e505056f1fc2ac64e5fcc7cb3c58b784d
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2026-05-11 22:57:55 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, (a, b) -> (a.start - b.start));
+
+        int end = -1;
+        for(Interval interval : intervals) {
+            if(end > interval.start) {
+                return false;
+            }
+            end = interval.end;
+        }
+        return true;
+    }
+}
