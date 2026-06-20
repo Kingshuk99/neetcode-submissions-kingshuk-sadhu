08aa26b7e514467b167c49b87c03865f1d79202f
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2026-06-20 20:51:53 +0530

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        //Recursion
+        List<String> tokenList = new ArrayList<>(Arrays.asList(tokens));
+        return dfs(tokenList);
+    }
+
+    private int dfs(List<String> tokenList) {
+        String token = tokenList.remove(tokenList.size() - 1);
+
+        if(! "+-*/".contains(token)) {
+            return Integer.parseInt(token);
+        }
+
+        int right = dfs(tokenList);
+        int left = dfs(tokenList);
+
+        return switch(token) {
+            case "+" -> left + right;
+            case "-" -> left - right;
+            case "*" -> left * right;
+            case "/" -> left / right;
+            default -> 0;
+        };
+    }
+}
