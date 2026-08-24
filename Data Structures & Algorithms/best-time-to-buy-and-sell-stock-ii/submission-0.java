88ac4942a6d6d9553e76d93a7d70c44aedf336ad
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2026-08-25 00:57:12 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // return dfs(prices, 0, false);
+        // int[][] dp = new int[prices.length + 1][2];
+        int prevBought = 0, currBought = 0, prevNotBought = 0, currNotBought = 0;
+
+        for(int i = prices.length - 1; i >= 0; i--) {
+            currBought = Math.max(prevBought, prevNotBought + prices[i]);
+            currNotBought = Math.max(prevNotBought, prevBought - prices[i]);
+            prevBought = currBought;
+            prevNotBought = currNotBought;
+        }
+        return prevNotBought;
+    }
+
+    // private int dfs(int[] prices, int ind, boolean bought) {
+    //     if(ind == prices.length) {
+    //         return 0;
+    //     }
+
+    //     if(bought) {
+    //         return Math.max(dfs(prices, ind + 1, bought), dfs(prices, ind + 1, !bought) + prices[ind]);
+    //     }
+    //     return Math.max(dfs(prices, ind + 1, bought), dfs(prices, ind + 1, !bought) - prices[ind]);
+    // }
+}
