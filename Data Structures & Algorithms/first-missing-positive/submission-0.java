7a293ecaf5e5a4e83c92869fce63de2b276a6cfc
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2026-08-25 21:13:44 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        // Cycle sort
+        int n = nums.length, i = 0;
+
+        while(i < n) {
+            if(nums[i] <= 0 || nums[i] > n) {
+                i++;
+                continue;
+            }
+
+            int ind = nums[i] - 1;
+            if(nums[i] != nums[ind]) {
+                int temp = nums[i];
+                nums[i] = nums[ind];
+                nums[ind] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        for(i = 0; i < n; i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
